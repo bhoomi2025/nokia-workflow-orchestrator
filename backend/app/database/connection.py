157272afa8b99873eb3c1f7abea085f6f9fe04f2
bhoomi2025 <bhoomisrivastava2023@gmail.com>
2026-07-29 01:18:41 +0530

    Refactor database connection to use env variables
    Refactored database connection to use environment variables for configuration and added SSL connection arguments.

--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -1,9 +1,19 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:12345@localhost/workflow_db"
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT", "4000")
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_NAME = os.environ.get("DB_NAME")
 
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"ssl": {"ssl_verify_cert": True, "ssl_verify_identity": True}}
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -14,6 +24,7 @@ SessionLocal = sessionmaker(
 Base = declarative_base()
 
 print("Database Connected Successfully!")
+
 def get_db():
     db = SessionLocal()
     try:
